b369973b5d5eea990c313f030bf2a6b622653e3c
samirls <samirlaguardiaii@gmail.com>
2023-04-05 11:52:17 -0300

    inserted render api for all requests

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const addItem = async (e) => {
     e.preventDefault();
     try{
-      const res = await axios.post('http://localhost:5500/api/item', {item: itemText})
+      const res = await axios.post('https://api-todo-familia.onrender.com/api/item', {item: itemText})
       setListItems(prev => [...prev, res.data]);
       setItemText('');
     }catch(err){
@@ -25,7 +25,7 @@ function App() {
   useEffect(()=>{
     const getItemsList = async () => {
       try{
-        const res = await axios.get('http://localhost:5500/api/items')
+        const res = await axios.get('https://api-todo-familia.onrender.com/api/items')
         setListItems(res.data);
         console.log('render')
       }catch(err){
@@ -38,7 +38,7 @@ function App() {
   // Delete item when click on delete
   const deleteItem = async (id) => {
     try{
-      const res = await axios.delete(`http://localhost:5500/api/item/${id}`)
+      const res = await axios.delete(`https://api-todo-familia.onrender.com/api/item/${id}`)
       const newListItems = listItems.filter(item=> item._id !== id);
       setListItems(newListItems);
     }catch(err){
